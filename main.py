a5f7f9a615b122a68d921f7497c1467f212831c6
Jamie Kim <jamiejiheonkim@gmail.com>
2021-01-13 16:21:42 -0700

    modified python script

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 
-response = requests.get('http://google.com')
+response = requests.get('http://raw.githubusercontent.com/JamieJiHeonKim/CMPUT404/master/main.py')
 
 #print('Your IP is {0}'.format(response.json()['origin']))
 
 #print(requests.__version__)
 
-print(response)
+print(response.content)
